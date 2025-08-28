fa1d22b65e4ff2a394010e5e32b656ad05397f7f
Benjamin Appiah <ben.appiah451@gmail.com>
2025-08-28 14:16:55 +0000

    Corrections made

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,5 +1,4 @@
-import { Text, View } from "react-native";
-import { StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
